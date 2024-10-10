ad6c2c3eb6f7d7a4508f65b1183ea77e8ac4a4ba
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2024-10-10 07:49:23 +0700

    build variant

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,20 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+}
+
+val apikeyPropertiesFile = rootProject.file("apikey.properties")
+val apikeyProperties = Properties()
+
+if (apikeyPropertiesFile.exists()) {
+    apikeyProperties.load(FileInputStream(apikeyPropertiesFile))
+} else {
+    println("Warning: apikey.properties file not found")
 }
 
 android {
@@ -15,8 +27,31 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    flavorDimensions.add("environment")
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            buildConfigField("String", "BASE_URL", "\"${apikeyProperties.getProperty("BASE_URL_DEV")}\"")
+            buildConfigField("String", "ACCESS_TOKEN", "\"${apikeyProperties.getProperty("ACCESS_TOKEN_DEV")}\"")
+        }
+        create("staging") {
+            dimension = "environment"
+            buildConfigField("String", "BASE_URL", "\"${apikeyProperties.getProperty("BASE_URL_STAGING")}\"")
+            buildConfigField("String", "ACCESS_TOKEN", "\"${apikeyProperties.getProperty("ACCESS_TOKEN_STAGING")}\"")
+        }
+        create("live") {
+            dimension = "environment"
+            buildConfigField("String", "BASE_URL", "\"${apikeyProperties.getProperty("BASE_URL_LIVE")}\"")
+            buildConfigField("String", "ACCESS_TOKEN", "\"${apikeyProperties.getProperty("ACCESS_TOKEN_LIVE")}\"")
+        }
     }
 
     buildTypes {
@@ -28,6 +63,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -39,11 +75,12 @@ android {
         dataBinding = true
     }
 }
+
 val lifecycle_version = "2.8.6"
 val arch_version = "2.2.0"
 val room_version = "2.6.1"
-dependencies {
 
+dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -56,18 +93,17 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     // Lifecycles only (without ViewModel or LiveData)
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
-    //Room
+    // Room
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     // To use Kotlin annotation processing tool (kapt)
     kapt("androidx.room:room-compiler:$room_version")
-    //Dagh hilt
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
-    //Retrofit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
-
 }
